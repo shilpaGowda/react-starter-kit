29fe3d4bff1f3e4d91f0a04575cd715a11fc6ebf
Shilpa Gowda <shilpavp229@gmail.com>
2021-04-29 10:06:04 +0530

    Initialize feature/bootstrap3

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+} from 'react-bootstrap';
+import Link from '../Link';
+import s from './Header.css';
+
+function Header() {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link className={s.brand} to="/">Your Company</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default withStyles(s)(Header);
